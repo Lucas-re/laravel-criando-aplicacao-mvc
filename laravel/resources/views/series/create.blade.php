54a4f6652d1ffe8f08ecb316b02ab5f1f4e5184c
Lucas Reis <gabriellucasjr017@gmail.com>
2024-09-19 23:38:49 -0300

    Laravel Mix: Criando o frontend da aplicação e instalando o bootstrap

--- laravel/resources/views/series/create.blade.php
+++ laravel/resources/views/series/create.blade.php
@@ -1,8 +1,12 @@
 <x-layout title="Nova Série">
 
     <form action="" method="post">
-        <label for="nome">Nome:</label>
-        <input type="text" name="nome" id="nome">
+        <div class="mb-3">
+            <label class="form-label" for="nome">Nome:</label>
+            <input class="form-control" type="text" name="nome" id="nome">
+        </div>
+        <button class="btn btn-primary" type="submit">Adicionar</button>
+        <a class="btn btn-secondary" href="/series/">Voltar</a>
     </form>
 
 </x-layout>
